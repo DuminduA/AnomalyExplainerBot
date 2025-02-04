3daa8c64e64d68cc85082f803aaaed7e48c487e8
DuminduA <duminduakalanka@gmail.com>
2025-02-04 03:04:38 +0200

    paths for saved model and tokenizer

--- uploader/views.py
+++ uploader/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+import os
 
 def upload(request):
     from transformers import AutoModelForCausalLM, AutoTokenizer
 
     # Specify the model directory (where you saved your trained model)
     model_path = "/Users/duminduakalanka/Documents/Oulu/Thesis/saved_model"
+    model_path = os.path.expanduser("~/saved_model")
+    tokenizer_path = os.path.expanduser("~/saved_tokenizer")
+
+    # Load the tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
 
     # Load the tokenizer
     tokenizer = AutoTokenizer.from_pretrained("/Users/duminduakalanka/Documents/Oulu/Thesis/saved_tokenizer")
