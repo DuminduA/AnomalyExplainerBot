c3a0f015385a4ad6d4134e9de24815fac7270e3a
DuminduA <duminduakalanka@gmail.com>
2025-02-13 06:34:15 +0200

    Create Module for predictions

---
+++ predictions/predict_results.py
@@ -0,0 +1,40 @@
+import os
+import torch
+import logging
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+from django.conf import settings
+from huggingface_hub import login
+from transformers import RobertaTokenizerFast,RobertaForSequenceClassification
+
+torch.device("cpu")
+
+logger = logging.getLogger(__name__)
+
+class AnomalyDetectionRobertaModel:
+    def __init__(self):
+        self.model, self.tokenizer = self.load_model()
+
+    def load_model(self):
+        login(settings.HUGGING_FACE_WRITE_API_KEY)
+
+        model = RobertaForSequenceClassification.from_pretrained("Dumi2025/log-anomaly-detection-model")
+        tokenizer = RobertaTokenizerFast.from_pretrained("Dumi2025/log-anomaly-detection-model")
+
+        model.to("cpu")
+        model.eval()
+
+        return model, tokenizer
+
+
+    def classify_log(self, log):
+        inputs = self.tokenizer(log, return_tensors="pt", truncation=True, padding=True)
+
+        # Perform classification
+        with torch.no_grad():
+            outputs = self.model(**inputs)
+            logits = outputs.logits
+            predicted_class = torch.argmax(logits, dim=-1).item()
+
+        return predicted_class
+
--- uploader/views.py
+++ uploader/views.py
@@ -1,42 +1,4 @@
-import logging
-import os
-
-from huggingface_hub import login
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-
-os.environ["CUDA_VISIBLE_DEVICES"] = ""
-
-import torch
 from django.shortcuts import render
-from django.conf import settings
-
-logger = logging.getLogger(__name__)
-
-torch.device("cpu")
 
 def upload(request):
-    login(settings.HUGGING_FACE_WRITE_API_KEY)
-
-    model = AutoModelForSequenceClassification.from_pretrained("Dumi2025/log-anomaly-detection-model")
-    tokenizer = AutoTokenizer.from_pretrained("Dumi2025/log-anomaly-detection-model")
-
-    model.to("cpu")
-    model.eval()
-
-    # Sample input text
-    input_text = "10.251.30.85:50010 Starting thread to transfer block blk_-7057732666118938934 to 10.251.106.214:50010"
-
-    # Tokenize the input
-    inputs = tokenizer(input_text, return_tensors="pt", truncation=True, padding=True)
-
-    # Perform classification
-    with torch.no_grad():
-        outputs = model(**inputs)
-        logits = outputs.logits  # Get raw scores
-        predicted_class = torch.argmax(logits, dim=-1).item()  # Get class index
-
-    # Print results
-    logger.info(f"Predicted Class: {predicted_class}")
-
-
     return render(request, 'uploader/uploader.html')
