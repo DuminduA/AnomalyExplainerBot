3f9dfd3adcfe5c5b86bb1f8b22567853efeb8a98
DuminduA <duminduakalanka@gmail.com>
2025-02-19 20:58:10 +0200

    new app chatusers

---
+++ chatusers/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+
+from django.contrib import messages
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'home/home.html', {})
+        else:
+            return render(request, 'forbidden.html', {})
+    return render(request, 'login.html', {})
+
+
+
