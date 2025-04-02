a7f7a3f58ccfe709ebfa6bd7096709c658ae0f14
DuminduA <duminduakalanka@gmail.com>
2025-04-03 01:36:03 +0300

    complete bertviz

---
+++ chat/static/js/visualize.js
@@ -0,0 +1,10 @@
+async function visualize_bert(){
+    window.location.href = 'bert-visualization';
+        // const response = await fetch('bert-visualization', {
+        //     method: 'GET',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'X-CSRFToken': getCSRFToken()
+        //     },
+        // });
+}
--- visualization/views.py
+++ visualization/views.py
@@ -1,23 +1,46 @@
 from django.shortcuts import render
-from bertviz import head_view
+from bertviz import head_view, model_view
 from uploader.views import UploaderViewSet
 
 model = UploaderViewSet.model
 
 def bert_attention_view(request):
     if request.method == "GET":
+        print("Bertviz visualization...")
         if len(model.attentions):
-            inputs = model.attentions[0]['inputs']
-            attentions = model.attentions[0]['attentions']
-            html = get_bertviz_visualizations(attentions, inputs)
 
-            html_str = html.__html__()
-            return render(request, "visualizations/bertviz.html", {"graphs": html_str})
-        return render(request, "visualizations/bertviz.html", {"graphs": "<h1>Could not generate the graphs</h1>"})
+            html_str_collection = []
+            model_view_str_collection = []
+            logs = []
+
+            for a in model.attentions:
+                inputs = a['inputs']
+                attentions = a['attentions']
+
+                html, tokens = get_bertviz_visualizations(attentions, inputs)
+
+                html_str = html._repr_html_()
+
+                model_html = get_model_visualization(attentions, inputs)
+
+                model_html_str = model_html._repr_html_()
+
+                html_str_collection.append(html_str)
+                model_view_str_collection.append(model_html_str)
+                logs.append(tokens)
+
+            return render(request, "visualizations/bertviz.html", {"graphs": html_str_collection, 'model_view': model_view_str_collection, 'logs': logs})
+        return render(request, "visualizations/bertviz.html", {"graphs": "<h1>Could not generate the graphs</h1>", 'model_view': "", 'logs': ""})
 
 def get_bertviz_visualizations(attentions, inputs):
     tokens = model.tokenizer.convert_ids_to_tokens(inputs.get('input_ids')[0])
 
     html = head_view(attentions, tokens, html_action="return")
+    return html, ' '.join(tokens)
+
+def get_model_visualization(attentions, inputs):
+    tokens = model.tokenizer.convert_ids_to_tokens(inputs.get('input_ids')[0])
+
+    html = model_view(attentions, tokens, html_action="return")
     return html
 
